0a5ab5c403e17de1758f55b12e198784425b7e3b
Chuka Okoye <okoye9@hotmail.com>
2010-06-22 07:58:54 -0500

    added opinion miner

---
+++ sentimentengine/opinionminer.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#
+# Copyright (c) 2010 Okoye Chuka D.<okoye9@gmail.com>
+#                    All rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from nltk.corpus import movie_reviews
+
+adjectives = []
+adverbs = []
+
+class OpinionMiner:
+
+   def __init__(self):
+      self.classifier = None
+      self._setInformativeFeatures()
+
+   @classmethod
+   def _setInformativeFeatures(self):
+      _setCommonAdjectives()
+      _setCommonAdverbs()
+
+   def _setCommonAdjectives(self):
+      
+
+   def train(self):
+      adjectives = _computeFreqAdjectives()
+
+   def _computeFreqAdjectives(self):
+      '''returns the most frequent adjectives used from the corpus'''
+      for sentence in movie_reviews.sents():
+         
